718183084a537a424970d914a7f2c6d592f53881
Adventure <m15583730895@163.com>
2020-04-03 07:44:53 +0800

    补签请求抓包

--- test.py
+++ test.py
@@ -7,4 +7,4 @@ ts = response.tx_time 
 _date = time.strftime('%Y-%m-%d',time.localtime(ts)) 
 _time = time.strftime('%X',time.localtime(ts)) 
 # os.system('date {} && time {}'.format(_date,_time)) 
-print('date {} && time {}'.format(_date,_time))
+# print('date {} && time {}'.format(_date,_time))
